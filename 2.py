a1d223c8f9742325128cb74effe0ff6ba8931c8b
meredith <547906080@qq.com>
2020-12-19 13:14:39 +0800

    完成第二题

--- 2.py
+++ 2.py
@@ -9,18 +9,72 @@
 
 k = 3
 chess_board = [[-1 for i in range(2 ** k)] for j in range(2 ** k)]  # 二维列表渲染
+missing_point = {'x': 6, 'y': 7}
+chess_board[missing_point['x']][missing_point['y']] = 0  # 标记缺少的位置
+step = 1  # 人工标记的特殊点
 
 
 # 展示棋盘
 def print_chess_board():
     global chess_board
-    chess_board[0][0] = 100
+    mat = "{:^3}"
     for i in chess_board:
         for j in i:
-            print(j, end=' ')
+            print(mat.format(j), end=' ')
         print()
 
 
-chess_board[7][6] = 0  # 标记缺少的位置
+# size 指的是棋盘的大小
+# chess_board_x 指的是当前棋盘左上的行的坐标
+# chess_board_y 指的是当前棋盘左上的列的坐标
+# missing_x 指的是缺失的格子的横坐标
+# missing_y 指的是缺失的格子的纵坐标
+def play_chess(size, chess_board_x, chess_board_y, missing_x, missing_y):
+    global step
+    # 设立递归的边界
+    if size == 1:
+        return
+    # 追踪的记号
+    trace_step = step
+    step += 1
+    size = int(size/2)
+    # 左上棋盘处理
+    if missing_x - chess_board_x < size and missing_y - chess_board_y < size:
+        # 缺失的棋子在处理的范围之内
+        play_chess(size, chess_board_x, chess_board_y, missing_x, missing_y)
+    else:
+        # 缺失的棋子不在处理的范围之内
+        chess_board[chess_board_x + size - 1][chess_board_y + size - 1] = trace_step
+        play_chess(size, chess_board_x, chess_board_y, chess_board_x + size - 1, chess_board_y + size - 1)
+
+    # 右上棋盘处理
+    if missing_x - chess_board_x < size and missing_y - chess_board_y >= size:
+        # 缺失的棋子在处理的范围之内
+        play_chess(size, chess_board_x, chess_board_y + size, missing_x, missing_y)
+    else:
+        # 缺失的棋子不在处理的范围之内
+        chess_board[chess_board_x + size - 1][chess_board_y + size] = trace_step
+        play_chess(size, chess_board_x, chess_board_y + size, chess_board_x + size - 1, chess_board_y + size)
+
+    # 左下棋盘处理
+    if missing_x - chess_board_x >= size and missing_y - chess_board_y < size:
+        # 缺失的棋子在处理的范围之内
+        play_chess(size, chess_board_x + size, chess_board_y, missing_x, missing_y)
+    else:
+        # 缺失的棋子不在处理的范围之内
+        chess_board[chess_board_x + size][chess_board_y + size - 1] = trace_step
+        play_chess(size, chess_board_x + size, chess_board_y, chess_board_x + size, chess_board_y + size - 1)
+
+    # 右下棋盘处理
+    if missing_x - chess_board_x >= size and missing_y - chess_board_y >= size:
+        # 缺失的棋子在处理的范围之内
+        play_chess(size, chess_board_x + size, chess_board_y + size, missing_x, missing_y)
+    else:
+        # 缺失的棋子不在处理的范围之内
+        chess_board[chess_board_x + size][chess_board_y + size] = trace_step
+        play_chess(size, chess_board_x + size, chess_board_y + size, chess_board_x + size, chess_board_y + size)
 
 
+play_chess(2**k, 0, 0, missing_point['x'], missing_point['y'])
+
+print_chess_board()
